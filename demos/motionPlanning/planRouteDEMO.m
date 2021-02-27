d73d7d6be69dba8b0993b96e67eed16136493141
Daniel <40699405+danjwilks@users.noreply.github.com>
2021-02-27 02:19:34 +0000

    Slam and Motion Planning Demos (#1)
    Created demos for the SLAM and Motion Planning parts of the system

---
+++ demos/motionPlanning/planRouteDEMO.m
@@ -0,0 +1,77 @@
+occGrid = load("occupancyGrid.mat", "map").map;
+show(occGrid)
+
+% Set the start and goal poses
+start = [-7.5, -2.5, -pi];
+goal = [7.5, -2, 0];
+
+
+% Show the start and goal positions of the robot
+hold on
+plot(start(1), start(2), 'ro')
+plot(goal(1), goal(2), 'mo')
+
+% Show the start and goal headings
+r = 0.5;
+plot([start(1), start(1) + r*cos(start(3))], [start(2), start(2) + r*sin(start(3))], 'r-' )
+plot([goal(1), goal(1) + r*cos(goal(3))], [goal(2), goal(2) + r*sin(goal(3))], 'm-' )
+hold off
+
+% Specify the state space of the vehicle using a stateSpaceDubins object 
+% and specifying the state bounds. This object limits the sampled states 
+% to feasible Dubins curves for steering a vehicle within the state 
+% bounds. A turning radius of 0.4m allows for tight turns in this small 
+% environment.
+bounds = [occGrid.XWorldLimits; occGrid.YWorldLimits; [-pi pi]];
+
+ss = stateSpaceDubins(bounds);
+ss.MinTurningRadius = 0.4;
+
+% To plan a path, the RRT algorithm samples random states within the 
+% state space and attempts to connect a path. These states and connections
+% need to be validated or excluded based on the map constraints. The 
+% vehicle must not collide with obstacles defined in the map.
+
+% Create a validatorOccupancyMap object with the specified state space. 
+% Set the Map property to the loaded occupancyMap object. Set a 
+% ValdiationDistance of 0.05m. This distance discretizes the path 
+% connections and checks obstacles in the map based on this.
+stateValidator = validatorOccupancyMap(ss); 
+stateValidator.Map = occGrid;
+stateValidator.ValidationDistance = 0.05;
+
+% Create the path planner and increase the max connection distance to 
+% connect more states. Set the maximum number of iterations for sampling 
+% states.
+planner = plannerRRT(ss, stateValidator);
+planner.MaxConnectionDistance = 2.0;
+planner.MaxIterations = 30000;
+
+% Customize the GoalReached function. This example helper function checks 
+% if a feasible path reaches the goal within a set threshold. The 
+% function returns true when the goal has been reached, and the planner 
+% stops.
+planner.GoalReachedFcn = @goalReachedFcn;
+
+% Plan the path between the start and goal. Because of the random 
+% sampling, this example sets the rng seed for consistent results.
+rng(0,'twister')
+
+[pthObj, solnInfo] = plan(planner, start, goal);
+
+% Show the occupancy map. Plot the search tree from the solnInfo. 
+% Interpolate and overlay the final path.
+show(occGrid)
+hold on
+
+% Search tree
+plot(solnInfo.TreeData(:,1), solnInfo.TreeData(:,2), '.-');
+
+% Interpolate and plot path
+interpolate(pthObj,300)
+plot(pthObj.States(:,1), pthObj.States(:,2), 'r-', 'LineWidth', 2)
+
+% Show the start and goal in the grid map
+plot(start(1), start(2), 'ro')
+plot(goal(1), goal(2), 'mo')
+hold off
---
+++ demos/slam/firstLoopClosureDEMO.m
@@ -0,0 +1,63 @@
+% Load a down-sampled data set consisting of laser scans collected from 
+% a mobile robot in an indoor environment. The average displacement 
+% between every two scans is around 0.6 meters. The offlineSlamData.mat 
+% file contains the scans variable
+% TODO: get our own data from webots, use for testing.
+load('offlineSlamData.mat');
+
+
+% Create a lidarSLAM object and set the map resolution and the max lidar 
+% range. This example uses a Jackal™ robot from Clearpath Robotics™. The 
+% robot is equipped with a SICK™ TiM-511 laser scanner with a max range 
+% of 10 meters. Set the max lidar range slightly smaller than the max 
+% scan range (8m), as the laser readings are less accurate near max 
+% range. Set the grid map resolution to 20 cells per meter, which gives 
+% a 5cm precision.
+% TODO: change from Jackal robot to something we can use in webots 
+% and something we can actually afford? Maybe we can build our software
+% and make it so it generalised to any robot we use that has radar?
+% We would just have to calibrate lidar readings differently? Look into 
+% this.
+maxLidarRange = 8;
+mapResolution = 20;
+slamAlg = lidarSLAM(mapResolution, maxLidarRange);
+
+% The following loop closure parameters are set empirically. Using higher
+% loop closure threshold helps reject false positives in loop closure 
+% identification process.
+slamAlg.LoopClosureThreshold = 210;  
+slamAlg.LoopClosureSearchRadius = 8;
+
+% Observe the Effect of Loop Closures and the Optimization Process
+% Continue to add scans in a loop. Loop closures should be automatically 
+% detected as the robot moves. Pose graph optimization is performed 
+% whenever a loop closure is identified. The output optimizationInfo has
+% a field, IsPerformed, that indicates when pose graph optimization 
+% occurs..
+
+% Plot the scans and poses whenever a loop closure is identified and 
+% verify the results visually. This plot shows overlaid scans and an 
+% optimized pose graph for the first loop closure. A loop closure edge is 
+% added as a red link.
+firstTimeLCDetected = false;
+
+figure;
+for i=10:length(scans)
+    [isScanAccepted, loopClosureInfo, optimizationInfo] = addScan(slamAlg, scans{i});
+    if ~isScanAccepted
+        continue;
+    end
+    % visualize the first detected loop closure, if you want to see the
+    % complete map building process, remove the if condition below
+    show(slamAlg, 'Poses', 'off');
+    hold on;
+    show(slamAlg.PoseGraph);
+    hold off;
+    drawnow
+    if optimizationInfo.IsPerformed && ~firstTimeLCDetected
+        firstTimeLCDetected = true;
+        break
+    end
+    
+end
+title('First loop closure');
