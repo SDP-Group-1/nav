4c166698881f64c25f971a2efd5ec05bc25b0b4c
Daniel <s1851664@ed.ac.uk>
2021-03-09 20:58:50 +0000

    updated folder structure

--- webots/basic-example/controllers/basic_movement/basic_movement.m
+++ webots/basic-example/controllers/basic_movement/basic_movement.m
@@ -4,12 +4,19 @@
 
 TIME_STEP = 64;
 
+BASE_SPEED = 1.5;
+
 % get the motor devices
 left_motor = wb_robot_get_device('left wheel motor');
 right_motor = wb_robot_get_device('right wheel motor');
+
 % set the target position of the motors
-wb_motor_set_position(left_motor, 10.0);
-wb_motor_set_position(right_motor, 10.0);
+wb_motor_set_position(left_motor, Inf);
+wb_motor_set_position(right_motor, Inf);
+
+% set up the motor speeds at 10% of the MAX_SPEED.
+wb_motor_set_velocity(left_motor, 1);
+wb_motor_set_velocity(right_motor, 2);
 
 % main loop:
 % perform simulation steps of TIME_STEP milliseconds
@@ -17,16 +24,8 @@ wb_motor_set_position(right_motor, 10.0);
 %
 while wb_robot_step(TIME_STEP) ~= -1
 
-  % read the sensors, e.g.:
-  %  rgb = wb_camera_get_image(camera);
-
-  % Process here sensor data, images, etc.
-
-  % send actuator commands, e.g.:
-  %  wb_motor_set_postion(motor, 10.0);
-
-  % if your code plots some graphics, it needs to flushed like this:
-  % drawnow;
+  wb_motor_set_velocity(left_motor, 1);
+  wb_motor_set_velocity(right_motor, 2);
 
 end
 
