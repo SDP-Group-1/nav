3211c21ff4dcba764ec3fdbbbb8abd381e4c7463
Daniel <40699405+danjwilks@users.noreply.github.com>
2021-03-14 17:21:22 +0000

    Basic functionality for occupancy grids (#6)
    * added some functionality
    
    * occupancy progress for sharing
    
    * added easy script for visualising the slamAlg
    
    * added pose graph opt - fix
    
    * fixed lidar interpretation issue
    
    * added .wbproj files
    
    * increase room size
    
    * removed *.wbproj from gitignore
    
    * cleaned up files
    
    Co-authored-by: Ap0806 <57600509+Ap0806@users.noreply.github.com>

---
+++ webots/occupancy-grid/visualize_occupancy_grid.m
@@ -0,0 +1,29 @@
+load('./webotsLidarReadings.mat');
+load('offlineSlamData.mat');
+
+maxLidarRange = 3;
+mapResolution = 20;
+slamAlg = lidarSLAM(mapResolution, maxLidarRange);
+
+slamAlg.LoopClosureThreshold = 360;
+slamAlg.LoopClosureSearchRadius = 3;
+firstTimeLCDetected = false;
+
+figure;
+for i=1:length(webots_scans)
+    [isScanAccepted, loopClosureInfo, optimizationInfo] = addScan(slamAlg, webots_scans{i});
+    if ~isScanAccepted
+        continue;
+    end
+end
+title('First loop closure');
+
+[scans, optimizedPoses]  = scansAndPoses(slamAlg);
+map = buildMap(scans, optimizedPoses, mapResolution, maxLidarRange);
+
+figure; 
+show(map);
+hold on
+show(slamAlg.PoseGraph, 'IDs', 'off');
+hold off
+title('Occupancy Grid Map Built Using Lidar SLAM');
