d4fd9d049613c1d4fdcffb99165685ce3d1bb64e
kenza-amira <57840494+kenza-amira@users.noreply.github.com>
2021-03-27 14:26:26 +0100

    draft

--- webots/movement-control/controllers/movement_control/movement_control.m
+++ webots/movement-control/controllers/movement_control/movement_control.m
@@ -16,7 +16,6 @@ if robot_node == 0
   quit(1);
 end
 
-or_field = wb_supervisor_node_get_field(robot_node, 'rotation');
 
 % get the motor devices
 left_motor = wb_robot_get_device('left wheel motor');
@@ -30,25 +29,69 @@ wb_motor_set_position(right_motor, Inf);
 wb_motor_set_velocity(left_motor, LEFT_BASE_SPEED);
 wb_motor_set_velocity(right_motor, RIGHT_BASE_SPEED);
 
-target_alpha = 0.10;
+or_field = wb_supervisor_node_get_field(robot_node, 'rotation');
+trans_field = wb_supervisor_node_get_field(robot_node, 'translation');
+
+
+target_x = 1;
+target_z = -1.39709;
 
 while wb_robot_step(TIME_STEP) ~= -1
+  flag = 0;
+
+  % get current rotation
   x_y_z_alpha_array = wb_supervisor_field_get_sf_rotation(or_field);
   curr_alpha = x_y_z_alpha_array(4);
   
-  %Rounded to 2 decimals because otherwise it would be too slow and because target alpha is 0.10
-  if round(curr_alpha,2) ~= target_alpha
-  wb_console_print(sprintf('curr alpha: %g, target alpha: %g.\n', round(curr_alpha,2), target_alpha), WB_STDOUT);
-  wb_motor_set_velocity(left_motor, -0.01);
-  wb_motor_set_velocity(right_motor, 0.01);
-  else
-  wb_console_print(sprintf('curr alpha: %g, target alpha: %g.\n', round(curr_alpha,2), target_alpha), WB_STDOUT);
-  wb_console_print(sprintf('Continuing straight'), WB_STDOUT);
-  wb_motor_set_velocity(left_motor, LEFT_BASE_SPEED);
-  wb_motor_set_velocity(right_motor, RIGHT_BASE_SPEED);
+  % get current location
+  trans_values = wb_supervisor_field_get_sf_vec3f(trans_field);
+  curr_x = trans_values(1);
+  curr_z = trans_values(3);
+  
+  % get target alpha
+  num = curr_x*target_x + curr_z * target_z;
+  denom = sqrt(((curr_x)^2+(curr_z)^2)*((target_x)^2+(target_z)^2));
+  target_alpha = - acos(num/denom);
+  wb_console_print(sprintf('curr alpha: %g, target alpha: %g.\n', round(curr_alpha,3), round(target_alpha,3)), WB_STDOUT);
+
+  
+  if (((round(target_alpha,3) + 0.05)< round(curr_alpha,3)) && flag == 0)  || (((round(target_alpha,3) - 0.05) > round(curr_alpha,3))&& flag == 0)
+    if round(target_alpha,3) < round(curr_alpha,3)
+    wb_console_print(sprintf('curr alpha: %g, target alpha: %g.\n', round(curr_alpha,3), round(target_alpha,3)), WB_STDOUT);
+    wb_motor_set_velocity(left_motor, 0.01);
+    wb_motor_set_velocity(right_motor, -0.01);
+    else
+    wb_console_print(sprintf('curr alpha: %g, target alpha: %g.\n', round(curr_alpha,3), round(target_alpha,3)), WB_STDOUT);
+    wb_motor_set_velocity(left_motor, -0.01);
+    wb_motor_set_velocity(right_motor, 0.01);
+    end
+  else  
+    wb_console_print(sprintf('curr alpha: %g, target alpha: %g.\n', round(curr_alpha,3), round(target_alpha,3)), WB_STDOUT);
+    wb_console_print(sprintf('Continuing straight'), WB_STDOUT);
+    wb_motor_set_velocity(left_motor, LEFT_BASE_SPEED);
+    wb_motor_set_velocity(right_motor, RIGHT_BASE_SPEED);
+    flag = 1
+  end
+  
+  wb_console_print(sprintf('MY_ROBOT is at position (x=%g, z=%g)\n', curr_x, curr_z), WB_STDOUT);  
+  wb_console_print(sprintf('    target location x=%g, z=%g.\n', target_x, target_z), WB_STDOUT);  
+    
+  % calculate distance to move
+  curr_dist_to_move_x = target_x - curr_x;
+  curr_dist_to_move_z = target_z - curr_z;
+  wb_console_print(sprintf('    curr_dist_to_move x=%g, z=%g.\n', curr_dist_to_move_x, curr_dist_to_move_z), WB_STDOUT);  
+  
+  x_sq = power(curr_dist_to_move_x, 2);
+  z_sq = power(curr_dist_to_move_z, 2);
+  wb_console_print(sprintf('    squared x=%g, z=%g.\n', x_sq, z_sq), WB_STDOUT);  
+  
+  dist_to_move = sqrt(x_sq + z_sq);
+  wb_console_print(sprintf('    %g ditance left to move.\n', dist_to_move), WB_STDOUT);  
+  
+  if dist_to_move < 0.05
+    break;
   end
   
 end
-
 
 % cleanup code goes here: write data to files, etc.
